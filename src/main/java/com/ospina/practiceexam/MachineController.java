7099dfe7af192fab4ba76e29bc1cb352e4779cce
Christopher Ospina <ospicg@farmingdale.edu>
2020-12-09 15:33:01 -0500

    wrote controller methods for deposit, withdraw, save as and check balance as well as the menu methods.

--- src/main/java/com/ospina/practiceexam/MachineController.java
+++ src/main/java/com/ospina/practiceexam/MachineController.java
@@ -6,10 +6,16 @@
 package com.ospina.practiceexam;
 
 import Buisness.DebitCard;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.PrintStream;
+import java.util.Scanner;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
+import javafx.stage.FileChooser;
 
 /**
  *
@@ -46,6 +52,72 @@ public class MachineController
    @FXML
    private TextArea txtarea;
    
+   @FXML
+   protected void handleopenaction() throws FileNotFoundException
+   {
+       FileChooser filechooser = new FileChooser();
+       filechooser.setTitle("Open debit card JSON file");
+       File file = filechooser.showOpenDialog(null);
+       FileReader fr = new FileReader(file);
+       d.readJSON(fr);
+       txtarea.setText("Read succesful");
+   }
+   
+   @FXML
+   protected void handlesaveasaction() throws FileNotFoundException
+   {
+       FileChooser fileChooser = new FileChooser();
+       fileChooser.setTitle("Save as TimeSheet");
+       File file = fileChooser.showOpenDialog(null);
+       PrintStream ps = new PrintStream(file);
+       d.writeJSON(ps);
+       txtarea.setText("Save succesful");
+   }
+   
+   @FXML
+   protected void handlesaveaction() throws FileNotFoundException
+   {
+       FileChooser fileChooser = new FileChooser();
+       fileChooser.setTitle("Save as TimeSheet");
+       File file = fileChooser.showOpenDialog(null);
+       PrintStream ps = new PrintStream(file);
+       d.print(ps);
+       txtarea.setText("Save succesful");
+   }
+   
+   @FXML
+   protected void handleexit()
+   {
+       System.exit(0);
+   }
+   
+   @FXML
+   protected void handledepositaction()
+   {
+       double depositamt;
+       Scanner in = new Scanner(System.in);
+       System.out.println("How much would you like to deposit?");
+       depositamt = in.nextFloat();
+       d.Deposit(depositamt);
+       System.out.println("deposit complete");
+   }
+   
+   @FXML
+   protected void handlewithdrawaction()
+   {
+       double withdrawamt;
+       Scanner in = new Scanner(System.in);
+       System.out.println("How much would you like to withdraw?");
+       withdrawamt = in.nextFloat();
+       d.Withdraw(withdrawamt);
+       System.out.println("withdraw complete");
+   }
+   
+   @FXML
+   protected void handlecheckbal()
+   {
+       txtarea.setText(String.valueOf(d.getbalance()));
+   }
    
     
     
